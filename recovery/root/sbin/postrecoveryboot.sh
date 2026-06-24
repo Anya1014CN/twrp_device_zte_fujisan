12908156c7c454fe4ba083be38a7151848b9167b
Anya1014CN <han.cf@foxmail.com>
2026-06-24 22:33:44 +0800

    Disable integrated secondary touch in recovery

--- recovery/root/sbin/postrecoveryboot.sh
+++ recovery/root/sbin/postrecoveryboot.sh
@@ -3,5 +3,6 @@
 echo 200 > /sys/class/leds/lcd-backlight-2/brightness 2>/dev/null
 echo 0 > /sys/class/leds/lcd-backlight/brightness 2>/dev/null
 echo 0 > /sys/class/graphics/fb1/blank 2>/dev/null
+echo 0 > /proc/touchscreen/integrate_device_mode 2>/dev/null
 
 exit 0
