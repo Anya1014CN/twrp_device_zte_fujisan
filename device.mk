61df195695f916b20ebb06da7d48467ff7a71067
Anya1014CN <han.cf@foxmail.com>
2026-06-25 13:46:00 +0800

    Add Qualcomm decrypt blobs to recovery

--- device.mk
+++ device.mk
@@ -9,7 +9,23 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/recovery/root/system/usr/idc/synaptics_dsx.idc:root/system/usr/idc/synaptics_dsx.idc \
     $(LOCAL_PATH)/recovery/root/system/usr/idc/zte-touchscreen.idc:root/system/usr/idc/zte-touchscreen.idc \
     $(LOCAL_PATH)/recovery/root/system/usr/keylayout/gpio-keys.kl:root/system/usr/keylayout/gpio-keys.kl \
-    $(LOCAL_PATH)/recovery/root/system/usr/keylayout/qpnp_pon.kl:root/system/usr/keylayout/qpnp_pon.kl
+    $(LOCAL_PATH)/recovery/root/system/usr/keylayout/qpnp_pon.kl:root/system/usr/keylayout/qpnp_pon.kl \
+    $(LOCAL_PATH)/recovery/root/vendor/bin/qseecomd:root/vendor/bin/qseecomd \
+    $(LOCAL_PATH)/recovery/root/vendor/bin/hw/android.hardware.gatekeeper@1.0-service:root/vendor/bin/hw/android.hardware.gatekeeper@1.0-service \
+    $(LOCAL_PATH)/recovery/root/vendor/bin/hw/android.hardware.keymaster@3.0-service:root/vendor/bin/hw/android.hardware.keymaster@3.0-service \
+    $(LOCAL_PATH)/recovery/root/vendor/lib64/libQSEEComAPI.so:root/vendor/lib64/libQSEEComAPI.so \
+    $(LOCAL_PATH)/recovery/root/vendor/lib64/libdrmfs.so:root/vendor/lib64/libdrmfs.so \
+    $(LOCAL_PATH)/recovery/root/vendor/lib64/libdrmtime.so:root/vendor/lib64/libdrmtime.so \
+    $(LOCAL_PATH)/recovery/root/vendor/lib64/librpmb.so:root/vendor/lib64/librpmb.so \
+    $(LOCAL_PATH)/recovery/root/vendor/lib64/libssd.so:root/vendor/lib64/libssd.so \
+    $(LOCAL_PATH)/recovery/root/vendor/lib64/libtime_genoff.so:root/vendor/lib64/libtime_genoff.so \
+    $(LOCAL_PATH)/recovery/root/vendor/lib64/libkeymasterdeviceutils.so:root/vendor/lib64/libkeymasterdeviceutils.so \
+    $(LOCAL_PATH)/recovery/root/vendor/lib64/libkeymasterprovision.so:root/vendor/lib64/libkeymasterprovision.so \
+    $(LOCAL_PATH)/recovery/root/vendor/lib64/libkeymasterutils.so:root/vendor/lib64/libkeymasterutils.so \
+    $(LOCAL_PATH)/recovery/root/vendor/lib64/hw/android.hardware.gatekeeper@1.0-impl.so:root/vendor/lib64/hw/android.hardware.gatekeeper@1.0-impl.so \
+    $(LOCAL_PATH)/recovery/root/vendor/lib64/hw/android.hardware.keymaster@3.0-impl.so:root/vendor/lib64/hw/android.hardware.keymaster@3.0-impl.so \
+    $(LOCAL_PATH)/recovery/root/vendor/lib64/hw/gatekeeper.msm8996.so:root/vendor/lib64/hw/gatekeeper.msm8996.so \
+    $(LOCAL_PATH)/recovery/root/vendor/lib64/hw/keystore.msm8996.so:root/vendor/lib64/hw/keystore.msm8996.so
 
 PRODUCT_PACKAGES += \
     qcom_decrypt
