2e76b5bc90e1d4bfbc7262e9291123a8471e008a
Anya1014CN <han.cf@foxmail.com>
2026-06-25 12:20:24 +0800

    Run touch filtering as recovery service

--- device.mk
+++ device.mk
@@ -5,6 +5,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/recovery/root/ueventd.rc:root/ueventd.rc \
     $(LOCAL_PATH)/recovery/root/ueventd.qcom.rc:root/ueventd.qcom.rc \
     $(LOCAL_PATH)/recovery/root/sbin/postrecoveryboot.sh:root/sbin/postrecoveryboot.sh \
+    $(LOCAL_PATH)/recovery/root/sbin/touchfilter.sh:root/sbin/touchfilter.sh \
     $(LOCAL_PATH)/recovery/root/system/usr/idc/synaptics_dsx.idc:root/system/usr/idc/synaptics_dsx.idc \
     $(LOCAL_PATH)/recovery/root/system/usr/idc/zte-touchscreen.idc:root/system/usr/idc/zte-touchscreen.idc \
     $(LOCAL_PATH)/recovery/root/system/usr/keylayout/gpio-keys.kl:root/system/usr/keylayout/gpio-keys.kl \
