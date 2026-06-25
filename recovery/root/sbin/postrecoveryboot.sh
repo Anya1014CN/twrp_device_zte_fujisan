1db318d2fe2102fe9b2e4dd8add8ceb948476809
Anya1014CN <han.cf@foxmail.com>
2026-06-25 22:16:46 +0800

    Reassert main panel after recovery boot

--- recovery/root/sbin/postrecoveryboot.sh
+++ recovery/root/sbin/postrecoveryboot.sh
@@ -1,7 +1,24 @@
 #!/sbin/sh
 
+sleep 1
+
 echo 200 > /sys/class/leds/lcd-backlight-2/brightness 2>/dev/null
 echo 0 > /sys/class/leds/lcd-backlight/brightness 2>/dev/null
-echo 0 > /sys/class/graphics/fb1/blank 2>/dev/null
+echo 0 > /sys/class/graphics/fb0/blank 2>/dev/null
+echo 4 > /sys/class/graphics/fb1/blank 2>/dev/null
+echo 4 > /sys/class/graphics/fb2/blank 2>/dev/null
+echo 0 > /proc/touchscreen/integrate_device_mode 2>/dev/null
+rm /dev/input/event2 2>/dev/null
+rm /dev/input/mouse1 2>/dev/null
+
+sleep 1
+echo 200 > /sys/class/leds/lcd-backlight-2/brightness 2>/dev/null
+echo 0 > /sys/class/leds/lcd-backlight/brightness 2>/dev/null
+echo 0 > /sys/class/graphics/fb0/blank 2>/dev/null
+echo 4 > /sys/class/graphics/fb1/blank 2>/dev/null
+echo 4 > /sys/class/graphics/fb2/blank 2>/dev/null
+echo 0 > /proc/touchscreen/integrate_device_mode 2>/dev/null
+rm /dev/input/event2 2>/dev/null
+rm /dev/input/mouse1 2>/dev/null
 
 exit 0
