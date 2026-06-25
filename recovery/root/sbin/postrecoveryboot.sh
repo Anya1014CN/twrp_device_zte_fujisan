f5b4432408478657facad5cb48ac62f8bd7da99d
Anya1014CN <han.cf@foxmail.com>
2026-06-25 12:14:36 +0800

    Force-disable secondary touch during recovery boot

--- recovery/root/sbin/postrecoveryboot.sh
+++ recovery/root/sbin/postrecoveryboot.sh
@@ -1,17 +1,20 @@
 #!/sbin/sh
 
-sleep 1
-
 echo 200 > /sys/class/leds/lcd-backlight-2/brightness 2>/dev/null
 echo 0 > /sys/class/leds/lcd-backlight/brightness 2>/dev/null
 echo 0 > /sys/class/graphics/fb1/blank 2>/dev/null
-echo 0 > /proc/touchscreen/integrate_device_mode 2>/dev/null
-rm /dev/input/event2 2>/dev/null
-rm /dev/input/mouse1 2>/dev/null
 
-sleep 1
-echo 0 > /proc/touchscreen/integrate_device_mode 2>/dev/null
-rm /dev/input/event2 2>/dev/null
-rm /dev/input/mouse1 2>/dev/null
+(
+    i=0
+    while [ "$i" -lt 12 ]; do
+        echo 0 > /proc/touchscreen/integrate_device_mode 2>/dev/null
+        rm /dev/input/event2 2>/dev/null
+        rm /dev/input/mouse1 2>/dev/null
+        rm /dev/input/event3 2>/dev/null
+        rm /dev/input/mouse2 2>/dev/null
+        sleep 1
+        i=$((i + 1))
+    done
+) &
 
 exit 0
