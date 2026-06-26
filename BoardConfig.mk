1caee0cffcc8c9bfa59ab1cf51afc72128008fb0
Anya1014CN <han.cf@foxmail.com>
2026-06-26 20:27:20 +0800

    Use framebuffer blanking for screen off

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -55,7 +55,6 @@ TW_THEME := portrait_hdpi
 TW_DEFAULT_LANGUAGE := zh_CN
 TW_EXTRA_LANGUAGES := false
 TW_SCREEN_BLANK_ON_BOOT := false
-TW_NO_SCREEN_BLANK := true
 TW_NO_SCREEN_TIMEOUT := true
 TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
 TW_MAX_BRIGHTNESS := 255
