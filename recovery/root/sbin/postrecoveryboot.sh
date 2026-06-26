ba1393bb068e001893cab135828ffd5a49863bc5
Anya1014CN <han.cf@foxmail.com>
2026-06-26 19:23:23 +0800

    Reassert main panel after decrypt page blanks

--- recovery/root/sbin/postrecoveryboot.sh
+++ recovery/root/sbin/postrecoveryboot.sh
@@ -21,4 +21,14 @@ echo 0 > /proc/touchscreen/integrate_device_mode 2>/dev/null
 rm /dev/input/event2 2>/dev/null
 rm /dev/input/mouse1 2>/dev/null
 
+sleep 5
+echo 200 > /sys/class/leds/lcd-backlight-2/brightness 2>/dev/null
+echo 0 > /sys/class/leds/lcd-backlight/brightness 2>/dev/null
+echo 0 > /sys/class/graphics/fb0/blank 2>/dev/null
+echo 4 > /sys/class/graphics/fb1/blank 2>/dev/null
+echo 4 > /sys/class/graphics/fb2/blank 2>/dev/null
+echo 0 > /proc/touchscreen/integrate_device_mode 2>/dev/null
+rm /dev/input/event2 2>/dev/null
+rm /dev/input/mouse1 2>/dev/null
+
 exit 0
